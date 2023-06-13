47cb4b58e450ea06ffe70c97518f3e4ddacb66d4
martgold123 <martgold321@gmail.com>
2023-06-13 09:27:03 +0700

    update

--- routes/user.apis.js
+++ routes/user.apis.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const userAccountController = require("../controllers/account/account.controller");
-const configController = require("../controllers/config/config.controller");
-const jobTrafficController = require("../controllers/jobtraffic/jobtraffic.controller");
-const coinController = require("../controllers/coin/coin.controller");
-const moneyController = require("../controllers/money/money.controller");
 const validateUser = require("../requests/validate.user");
 const isAuth = require("../utils/validatetoken.util");
 
